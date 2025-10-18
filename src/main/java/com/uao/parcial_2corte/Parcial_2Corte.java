43686a2ad5cdc4e191ea0ce9d35c8e148ad670ed
N1COLU <nicolas.castano@uao.edu.co>
2025-10-18 18:09:46 -0500

    Se comienza el parcial de segundo corte

---
+++ src/main/java/com/uao/parcial_2corte/Parcial_2Corte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uao.parcial_2corte;
+
+/**
+ *
+ * @author Nicolas Castaño
+ */
+public class Parcial_2Corte {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

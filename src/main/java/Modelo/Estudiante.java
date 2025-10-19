2b5f69c0e43cd29130a49f5003b872c010ac965e
N1COLU <nicolas.castano@uao.edu.co>
2025-10-18 19:04:53 -0500

    Se corrigió sintaxis de la clase Estudiante a clase Persona

--- src/main/java/Modelo/Estudiante.java
+++ src/main/java/Modelo/Estudiante.java
@@ -9,32 +9,5 @@ package Modelo;
  * @author Nicolas Castaño
  */
 public class Estudiante {
-    private String nombre;
-    private int numero;
-    private int dia;
-    private int mes;
-    private int año;
-    private String direccion;
-    
-    public Estudiante (String nombre,int numero, int dia, int mes, int año, String direccion ){
-        this.nombre = nombre;
-        this.numero = numero;
-        this.dia = dia;
-        this.mes = mes;
-        this.año = año;
-        this.direccion = direccion;
-    }
-    
-    
-    public String datosPersona (){
-        return "Nombre: " +nombre+ "\n Número de teléfono: " +numero+ "\n Fecha de nacimiento: "+dia+"/"+mes+"/"+año+ "\n Dirección: "+direccion;
-    }
-    
-    @Override
-    public String toString(){
-        return datosPersona();
-    }
-
-    
     
 }
